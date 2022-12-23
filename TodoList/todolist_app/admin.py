9e34c47c96b83d5e4638faf6feb05d76c1c5ae5c
White <thumarharshit123@gmail.com>
2022-12-23 14:55:30 +0530

    [Add]Add the tree view in the todoitem model in admin panel

--- TodoList/todolist_app/admin.py
+++ TodoList/todolist_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import TodoItem
 
-admin.site.register(TodoItem)
+
+@admin.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_completed')
