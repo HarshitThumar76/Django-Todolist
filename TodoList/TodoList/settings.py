b3120a8b1df444cd6a2cb2713be80b89282c5785
White <thumarharshit123@gmail.com>
2023-01-03 22:57:37 +0530

    [Add]Add the cache for the basic template

--- TodoList/TodoList/settings.py
+++ TodoList/TodoList/settings.py
@@ -81,6 +81,14 @@ DATABASES = {
     }
 }
 
+# Caches
+# https://docs.djangoproject.com/en/4.1/topics/cache/
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'todolist_cache',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
